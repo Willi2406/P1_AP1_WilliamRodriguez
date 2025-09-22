e69c6f5fe3f8d0f5b3f575116ddf98f046f0b904
William Alexander Rodriguez Valentin <william_rodriguez@ucne.edu.do>
2025-09-22 19:40:37 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_WilliamRodriguez.Models;
+
+namespace P1_AP1_WilliamRodriguez.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        
+        public DbSet<Registro> Registro { get; set; }  
+
+    }
+}
