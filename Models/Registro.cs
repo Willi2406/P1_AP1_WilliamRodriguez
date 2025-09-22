67f83ad77feabce79d7c62fa3391f55b751e742a
William Alexander Rodriguez Valentin <william_rodriguez@ucne.edu.do>
2025-09-22 19:34:26 -0400

    Models

---
+++ Models/Registro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_WilliamRodriguez.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int registroId {  get; set; }
+    }
+}
