907cd613ef8c16d2ed5fbe946bd0df097dbaf973
William Alexander Rodriguez Valentin <william_rodriguez@ucne.edu.do>
2025-09-22 20:23:53 -0400

    services

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using P1_AP1_WilliamRodriguez.DAL;
+using P1_AP1_WilliamRodriguez.Models;
+
+
+
+namespace P1_AP1_WilliamRodriguez.Services;
+
+public class RegistroServices (IDbContextFactory<Contexto> dbContextFactory)
+{
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+    {
+        using var ctx = await dbContextFactory.CreateDbContextAsync();
+        return await ctx.Registro
+                        .Where(criterio)
+                        .AsNoTracking()
+                        .ToListAsync();
+    }
+
+}
